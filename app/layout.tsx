e14050ad46946af04c7157e28b8a788879b175ce
fograi <dev@fogr.ai>
2025-03-06 19:05:42 -0500

    Trying to fix mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
         >
           <div className="relative flex flex-col h-screen">
             <Navbar />
-            <main className="mx-auto px-6 flex-grow">{children}</main>
+            <main>{children}</main>
             <footer className="w-full flex items-center justify-center py-3">
               <Link
                 isExternal
