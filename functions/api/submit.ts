fe0c07ff376d7a8ba875f1c6e35ad7b41c585492
fograi <dev@fogr.ai>
2025-03-07 17:43:55 -0500

    Trying with .js

--- functions/api/submit.ts
+++
@@ -1,8 +0,0 @@
-interface Env {
-  KV: KVNamespace;
-}
-
-export const onRequest: PagesFunction<Env> = async () =>
-  new Response(JSON.stringify({ error: "Random failure for testing." }), {
-    status: 500,
-  });
