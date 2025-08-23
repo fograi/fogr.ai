fa3bc94168e088361831c56195ecd58f0fb394e8
fograi <dev@fogr.ai>
2025-08-22 23:00:42 -0400

    Changing over to svelte

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-// import Loading from "@/components/loading";
-import ClassifiedMasonryGrid from "@/components/classifieds/masonry-grid";
-
-export default function ListingsPage() {
-  return <ClassifiedMasonryGrid />;
-}
--- constants/validations.ts
+++
@@ -1,7 +0,0 @@
-export const MIN_TITLE_LENGTH = 5;
-export const MAX_TITLE_LENGTH = 50;
-export const MIN_DESC_LENGTH = 10;
-export const MAX_DESC_LENGTH = 500;
-
-export const MAX_IMAGE_SIZE = 5 * 1024 * 1024; // 5MB
-export const ALLOWED_IMAGE_TYPES = ["image/jpeg", "image/png"];
