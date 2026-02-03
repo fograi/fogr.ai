d7f5913ee555cf39acb05dab00eabaa5715184b1
Martin Kilmartin <martinkilmartin@gmail.com>
2026-02-03 13:27:17 -0500

    feat: update load function to include E2E mock user support

--- src/routes/(app)/account/+page.server.ts
+++ src/routes/(app)/account/+page.server.ts
@@ -1,7 +1,12 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { E2E_MOCK_USER, isE2eMock } from '$lib/server/e2e-mocks';
 
-export const load: PageServerLoad = async ({ locals, url }) => {
+export const load: PageServerLoad = async ({ locals, url, platform }) => {
+	if (isE2eMock(platform)) {
+		return { user: E2E_MOCK_USER };
+	}
+
 	const user = await locals.getUser();
 	if (!user) {
 		throw redirect(303, `/login?redirectTo=${encodeURIComponent(url.pathname)}`);
