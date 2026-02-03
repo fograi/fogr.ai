526487050a3fe903355cf21888a460d5fab4f8c4
Martin Kilmartin <martinkilmartin@gmail.com>
2026-02-03 18:21:18 -0500

    feat: update playwright configuration to include timeout and server reuse options

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
 	webServer: {
 		command: 'svelte-kit sync && npm run build && wrangler dev --var E2E_MOCK:1 --port 8787',
-		port: 8787
+		port: 8787,
+		timeout: 120_000,
+		reuseExistingServer: true
 	},
 	testDir: 'e2e'
 });
